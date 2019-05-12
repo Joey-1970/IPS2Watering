e8165a981e920a8186c9a13b12da516b6d15134f
Joey-1970 <paeper@horburg.de>
2019-05-12 21:02:52 +0200

    Update module.php

--- IPS2WateringSwitch/module.php
+++ IPS2WateringSwitch/module.php
@@ -5,22 +5,35 @@
        
         // Überschreibt die interne IPS_Create($id) Funktion
         public function Create() {
-            // Diese Zeile nicht löschen.
-            parent::Create();
-            $this->RegisterPropertyBoolean("Open", false);
-            $this->RegisterPropertyInteger("ActuatorID", 0);
-            $this->RegisterPropertyInteger("SensorID", 0);
+            	// Diese Zeile nicht löschen.
+            	parent::Create();
+            	$this->RegisterPropertyBoolean("Open", false);
+            	$this->RegisterPropertyInteger("ActuatorID", 0);
+            	$this->RegisterPropertyInteger("SensorID", 0);
             
-            $this->RegisterVariableBoolean("Automatic", "Automatik", "~Switch", 10);
-		    $this->EnableAction("Automatic");
-            $this->RegisterVariableBoolean("State", "Status", "~Switch", 20);
-		    $this->EnableAction("State");
+            	$this->RegisterVariableBoolean("Automatic", "Automatik", "~Switch", 10);
+		$this->EnableAction("Automatic");
+            	$this->RegisterVariableBoolean("State", "Status", "~Switch", 20);
         }
  
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() {
-            // Diese Zeile nicht löschen
-            parent::ApplyChanges();
+            	// Diese Zeile nicht löschen
+            	parent::ApplyChanges();
+		$this->RegisterEvent("Wochenplan", "IPS2Watering_Event_".$this->InstanceID, 2, $this->InstanceID, 30);
+		If ($this->ReadPropertyBoolean("Automatic") == true) {
+			$this->EnDisableAction("State");
+		}
+		else {
+			$this->EnableAction("State");
+		}
+		
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$this->SetStatus(102);
+		}
+		else {
+			$this->SetStatus(104);
+		}
         }
  
         public function GetConfigurationForm() 
@@ -42,6 +55,27 @@
             $arrayActions[] = array("type" => "Label", "label" => "Diese Funktionen stehen erst nach Eingabe und Übernahme der erforderlichen Daten zur Verfügung!");
 
             return JSON_encode(array("status" => $arrayStatus, "elements" => $arrayElements, "actions" => $arrayActions)); 		 
-        }   
+        }  
+	    
+	    
+	private function RegisterEvent($Name, $Ident, $Typ, $Parent, $Position)
+	{
+		$eid = @$this->GetIDForIdent($Ident);
+		if($eid === false) {
+		    	$eid = 0;
+		} elseif(IPS_GetEvent($eid)['EventType'] <> $Typ) {
+		    	IPS_DeleteEvent($eid);
+		    	$eid = 0;
+		}
+		//we need to create one
+		if ($eid == 0) {
+			$EventID = IPS_CreateEvent($Typ);
+		    	IPS_SetParent($EventID, $Parent);
+		    	IPS_SetIdent($EventID, $Ident);
+		    	IPS_SetName($EventID, $Name);
+		    	IPS_SetPosition($EventID, $Position);
+		    	IPS_SetEventActive($EventID, true);  
+		}
+	}  
     }
 ?>
