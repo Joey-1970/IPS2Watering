61d8fa24f559713dca4ee54d5d4b98542d0f9c48
Joey-1970 <paeper@horburg.de>
2019-05-12 20:06:06 +0200

    Update module.php

--- IPS2WateringSwitch/module.php
+++ IPS2WateringSwitch/module.php
@@ -18,6 +18,25 @@
             parent::ApplyChanges();
         }
  
-        
+        public function GetConfigurationForm() 
+        { 
+            $arrayStatus = array(); 
+            $arrayStatus[] = array("code" => 101, "icon" => "inactive", "caption" => "Instanz wird erstellt"); 
+            $arrayStatus[] = array("code" => 102, "icon" => "active", "caption" => "Instanz ist aktiv");
+            $arrayStatus[] = array("code" => 104, "icon" => "inactive", "caption" => "Instanz ist inaktiv");
+
+            $arrayElements = array(); 
+            $arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv"); 
+            $arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
+            $arrayElements[] = array("type" => "Label", "label" => "Ventil-Aktor-Variable (Boolean)");
+            $arrayElements[] = array("type" => "SelectVariable", "name" => "ActuatorID", "caption" => "Aktor"); 
+             $arrayElements[] = array("type" => "Label", "label" => "Bodenfeuchtigkeits-Sensor-Variable (Float)");
+            $arrayElements[] = array("type" => "SelectVariable", "name" => "SensorID", "caption" => "Aktor"); 
+            $arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________"); 
+            $arrayActions = array();
+            $arrayActions[] = array("type" => "Label", "label" => "Diese Funktionen stehen erst nach Eingabe und Übernahme der erforderlichen Daten zur Verfügung!");
+
+            return JSON_encode(array("status" => $arrayStatus, "elements" => $arrayElements, "actions" => $arrayActions)); 		 
+        }   
     }
 ?>
