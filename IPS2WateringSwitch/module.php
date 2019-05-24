9cc3b5a8154b38f8fecad54563ed95367098ae5e
Joey-1970 <paeper@horburg.de>
2019-05-24 21:27:36 +0200

    Update module.php

--- IPS2WateringSwitch/module.php
+++ IPS2WateringSwitch/module.php
@@ -2,13 +2,6 @@
     // Klassendefinition
     class IPS2WateringSwitch extends IPSModule {
  	
-       public function Destroy() 
-	{
-		//Never delete this line!
-		parent::Destroy();
-		$this->SetTimerInterval("WeekplanState", 0);
-	}
-	    
         // Überschreibt die interne IPS_Create($id) Funktion
         public function Create() {
             	// Diese Zeile nicht löschen.
@@ -20,25 +13,11 @@
 		$this->RegisterPropertyInteger("MaxWatering", 30);
             	$this->RegisterPropertyInteger("MinWaitTime", 180);
 		
-		$this->RegisterEvent("Wochenplan", "IPS2Watering_Event_".$this->InstanceID, 2, $this->InstanceID, 30);
-		// Anlegen der Daten für den Wochenplan
-		for ($i = 0; $i <= 6; $i++) {
-			IPS_SetEventScheduleGroup($this->GetIDForIdent("IPS2Watering_Event_".$this->InstanceID), $i, pow(2, $i));
-		}
-		IPS_SetEventScheduleAction($this->GetIDForIdent("IPS2Watering_Event_".$this->InstanceID), 1, "Freigabe", 0x40FF00, "WateringSwitch_SetState(\$_IPS['TARGET'], 1);");	
-		IPS_SetEventScheduleAction($this->GetIDForIdent("IPS2Watering_Event_".$this->InstanceID), 2, "Sperrzeit", 0xFF0040, "WateringSwitch_SetState(\$_IPS['TARGET'], 1);");	
-
-		$this->RegisterTimer("WeekplanState", 0, 'WateringSwitch_TimerEventGetWeekplanState($_IPS["TARGET"]);'); 
-		
-            	$this->RegisterProfileInteger("IPS2Watering.WeekplanState", "Information", "", "", 0, 2, 1);
-		IPS_SetVariableProfileAssociation("IPS2Watering.WeekplanState", 0, "Undefiniert", "Warning", 0xFF0040);
-		IPS_SetVariableProfileAssociation("IPS2Watering.WeekplanState", 1, "Freigabe", "LockOpen", 0x40FF00);
-		IPS_SetVariableProfileAssociation("IPS2Watering.WeekplanState", 2, "Sperrzeit", "LockClosed", 0xFF0040);
 		
 		$this->RegisterVariableBoolean("Automatic", "Automatik", "~Switch", 10);
 		$this->EnableAction("Automatic");
             	$this->RegisterVariableBoolean("State", "Status", "~Switch", 20);	
-		$this->RegisterVariableInteger("WeekplanState", "Wochenplanstatus", "IPS2Watering.WeekplanState", 30);
+		
 		
         }
 	    
@@ -78,11 +57,7 @@
 			$this->RegisterMessage($this->ReadPropertyInteger("ActuatorID"), 10603);
 		}
 		
-		// Registrierung für die Änderung des Wochenplans
-		$WeekplanID = $this->GetIDForIdent("IPS2Watering_Event_".$this->InstanceID);
-		$this->RegisterMessage($WeekplanID, 10821);
-		$this->RegisterMessage($WeekplanID, 10822);
-		$this->RegisterMessage($WeekplanID, 10823);
+		
 		
 		
 		If (GetValueBoolean($this->GetIDForIdent("Automatic")) == true) {
@@ -94,12 +69,11 @@
 		
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			
-			$this->GetWeekplanState($WeekplanID);
-			$this->SetTimerInterval("WeekplanState", (30 * 1000));
+			
 			$this->SetStatus(102);
 		}
 		else {
-			$this->SetTimerInterval("WeekplanState", 0);
+			
 			$this->SetStatus(104);
 		}
         }
@@ -145,24 +119,7 @@
 					$this->SendDebug("MessageSink", "Ausloeser Aenderung Aktor-Status", 0);
 					
 				}
-			case 10821:
-				// Änderung des Wochenplans
-				$this->SendDebug("MessageSink", "Ausloeser Aenderung Wochenplan", 0);
-				$WeekplanID = $this->GetIDForIdent("IPS2Watering_Event_".$this->InstanceID);
-				$this->GetWeekplanState($WeekplanID);
-				break;
-			case 10822:
-				// Änderung des Wochenplans
-				$this->SendDebug("MessageSink", "Ausloeser Aenderung Wochenplan", 0);
-				$WeekplanID = $this->GetIDForIdent("IPS2Watering_Event_".$this->InstanceID);
-				$this->GetWeekplanState($WeekplanID);
-				break;
-			case 10823:
-				// Änderung des Wochenplans
-				$this->SendDebug("MessageSink", "Ausloeser Aenderung Wochenplan", 0);
-				$WeekplanID = $this->GetIDForIdent("IPS2Watering_Event_".$this->InstanceID);
-				$this->GetWeekplanState($WeekplanID);
-				break;
+			
 		}
     	}        
 	    
@@ -173,43 +130,7 @@
 		$this->GetWeekplanState($WeekplanID);
 	}
 	
-	public function TimerEventGetWeekplanState()
-	{  
-		$WeekplanID = $this->GetIDForIdent("IPS2Watering_Event_".$this->InstanceID);
-		$this->GetWeekplanState($WeekplanID);
-	}
-	    
-	private function GetWeekplanState(int $WeekplanID)
-	{
-		$this->SendDebug("GetWeekplanState", "Wochenplan Status einlesen", 0);
-		$e = IPS_GetEvent($WeekplanID);
-		$actionID = false;
-		//Durch alle Gruppen gehen
-		foreach($e['ScheduleGroups'] as $g) 
-			{
-			//Überprüfen ob die Gruppe für heute zuständig ist
-		    	if(($g['Days'] & pow(2,date("N",time())-1)) > 0)  
-			{
-				//Aktuellen Schaltpunkt suchen. Wir nutzen die Eigenschaft, dass die Schaltpunkte immer aufsteigend sortiert sind.
-				foreach($g['Points'] as $p) 
-				{
-			   		if(date("H") * 3600 + date("i") * 60 + date("s") >= $p['Start']['Hour'] * 3600 + $p['Start']['Minute'] * 60 + $p['Start']['Second']) 
-					{
-			      			$actionID = $p['ActionID'];
-			   		} 
-					else 
-					{
-			      			break; //Sobald wir drüber sind, können wir abbrechen.
-			   		}
-		       		}
-				break; //Sobald wir unseren Tag gefunden haben, können wir die Schleife abbrechen. Jeder Tag darf nur in genau einer Gruppe sein.
-		    	}
-		}
-		$this->SendDebug("GetWeekplanState", "Ergebnis: ".intval($actionID), 0);
-		If (GetValueInteger($this->GetIDForIdent("WeekplanState")) <> intval($actionID)) {
-			SetValueInteger($this->GetIDForIdent("WeekplanState"),  intval($actionID));
-		}
-	}
+	
 	
 	private function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
 	{
@@ -228,24 +149,6 @@
 	        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);        
 	}
   
-	private function RegisterEvent($Name, $Ident, $Typ, $Parent, $Position)
-	{
-		$eid = @$this->GetIDForIdent($Ident);
-		if($eid === false) {
-		    	$eid = 0;
-		} elseif(IPS_GetEvent($eid)['EventType'] <> $Typ) {
-		    	IPS_DeleteEvent($eid);
-		    	$eid = 0;
-		}
-		//we need to create one
-		if ($eid == 0) {
-			$EventID = IPS_CreateEvent($Typ);
-		    	IPS_SetParent($EventID, $Parent);
-		    	IPS_SetIdent($EventID, $Ident);
-		    	IPS_SetName($EventID, $Name);
-		    	IPS_SetPosition($EventID, $Position);
-		    	IPS_SetEventActive($EventID, true);  
-		}
-	}  
+	
 }
 ?>
