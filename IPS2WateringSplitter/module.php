b5fb95b669776ed42f8529cf3799d615d32a7210
Joey-1970 <paeper@horburg.de>
2019-07-03 22:12:30 +0200

    Update module.php

--- IPS2WateringSplitter/module.php
+++ IPS2WateringSplitter/module.php
@@ -115,14 +115,16 @@
 				  	SetValueInteger($this->GetIDForIdent($Ident), $Value);
 				  	If ($Value == 0) {
 					 	// Aus
-						$this->SendDataToChildren(json_encode(Array("DataID" => "{3AB3B462-743D-EA60-16E1-6EECEDD9BF16}", "Function"=>"set_State", "InstanceID" => 0, "State"=>0)));
+						$this->SendDataToChildren(json_encode(Array("DataID" => "{3AB3B462-743D-EA60-16E1-6EECEDD9BF16}", 
+											    "Function"=>"set_State", "InstanceID" => 0, "State"=>false)));
 				  	}
 			    	  	elseif ($Value == 1) {
 						// Programm
 					}
 					elseif ($Value >= 10000) {
 						// bestimmte Instanz
-						$this->SendDataToChildren(json_encode(Array("DataID" => "{3AB3B462-743D-EA60-16E1-6EECEDD9BF16}", "Function"=>"set_State", "InstanceID" =>$Value, "State"=>0)));
+						$this->SendDataToChildren(json_encode(Array("DataID" => "{3AB3B462-743D-EA60-16E1-6EECEDD9BF16}", 
+											    "Function"=>"set_State", "InstanceID" =>$Value, "State"=>true)));
 					}
 			    }
 			    break;	
