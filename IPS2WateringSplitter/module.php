5d0aabfd3477a62c7b51aaf942b09e591aad0443
Joey-1970 <paeper@horburg.de>
2020-07-21 07:30:42 +0200

    Update module.php

--- IPS2WateringSplitter/module.php
+++ IPS2WateringSplitter/module.php
@@ -75,9 +75,12 @@
 		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
             	$arrayElements[] = array("type" => "Label", "label" => "Minimum-Temperatur");
             	$arrayElements[] = array("type" => "NumberSpinner", "name" => "MinTemperature",  "caption" => "Grad (C°)");
-		$arrayActions = array();
-            	$arrayActions[] = array("type" => "Label", "label" => "Diese Funktionen stehen erst nach Eingabe und Übernahme der erforderlichen Daten zur Verfügung!");
-            	return JSON_encode(array("status" => $arrayStatus, "elements" => $arrayElements, "actions" => $arrayActions)); 		 
+		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
+		$arrayActions = array(); 
+		$arrayActions[] = array("type" => "Label", "label" => "Test Center"); 
+		$arrayActions[] = array("type" => "TestCenter", "name" => "TestCenter");
+		
+ 		return JSON_encode(array("status" => $arrayStatus, "elements" => $arrayElements, "actions" => $arrayActions)); 	
         }  
  
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
